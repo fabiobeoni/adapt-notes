6df3df10745e48304d8a9336f23c42406e1a132c
fbfb79 <reg@beoni.net>
2017-07-11 09:35:31 +0200

    Docs

--- js/adapt-notes.js
+++ js/adapt-notes.js
@@ -1,15 +1,33 @@
+/**
+ * This plugin registers an extension for Adapt
+ * providing a notes editor to the students taking
+ * the course.
+ * The editor list notes, edit them and search
+ * for them. Notes are stored in the browser
+ * localStorage, can be exported on text file
+ * on the device.
+ *
+ * Here is all about configuring the Adapt
+ * drawer object with minimum required
+ * data.
+ */
 define([
     'backbone',
     'coreJS/adapt',
     './adapt-notesView',
-], function(Backbone, Adapt, notesView) {
+], function(Backbone, Adapt, NotesView) {
 
+    //waits for data ready to be able to access
+    //plugin configuration into the course
     Adapt.once('app:dataReady', function() {
 
+        //reads the config of this plugin that
+        //the course author can change
         var notesExtConfig = Adapt.course.get('_notes');
-
         if(notesExtConfig._isEnabled === false) return;
 
+        //sets up the drawer with config title and description
+        //shown to the student
         var drawerObject = {
             title: notesExtConfig.title,
             description: notesExtConfig.description,
@@ -19,8 +37,11 @@ define([
         // Syntax for adding a Drawer item
         Adapt.drawer.addItem(drawerObject, 'adaptNotes:showContentObjects');
 
+        //registers the event listener to display
+        //the plugin custom UI into the drawer
+        var notesView = new NotesView();
         Adapt.on('adaptNotes:showContentObjects', function() {
-            Adapt.drawer.triggerCustomView(new notesView().$el);
+            Adapt.drawer.triggerCustomView(notesView.$el);
         });
     });
 
--- js/adapt-notesView.js
+++ js/adapt-notesView.js
@@ -1,21 +1,75 @@
+/**
+ * This view displays the NoteEditor.
+ * Normally this class should be a
+ * Backbone component, but since I
+ * want to reuse a notes editor I
+ * have already coded in JQuery,
+ * I'm just wrapping it into this
+ * Backbone view. Since it works
+ * independently from the Backbone
+ * view lifecycle, it runs on
+ * postRender event, when the
+ * Backbone template has been loaded
+ * and injected into the drawer view.
+ */
 define(function(require) {
 
     var Backbone = require('backbone');
     var Adapt = require('coreJS/adapt');
     var Base64 = require('./base64');
 
-    var $Notes = function (Adapt,selector) {
-
-        var _notesExtConfig={};
-        var _notes = [];
-        var _note = null;
+    /**
+     * Initializes the notes editor component
+     * that manages the all editing and presenting
+     * of notes.
+     * @param Adapt {object} the adapt configuration
+     * @param selector {string}
+     * @constructor
+     */
+    var NotesEditor = function (Adapt,selector) {
 
         var BASE64_PREFIX = 'data:;base64,';
+
+        /**
+         * The key in use to store notes in
+         * the browser local storage
+         * @type {string}
+         */
         var STORAGE_KEY = ('adaptNotes-'+Adapt.config.attributes._id);
 
+        /**
+         * AdaptNotes plugin configuration object
+         * set by the course author by the authoring
+         * tool.
+         * @type {object}
+         * @private
+         */
+        var _notesExtConfig={};
 
+        /**
+         * List of notes stored on device
+         * and displayed on view
+         * @type {Array}
+         * @private
+         */
+        var _notes = [];
+
+        /**
+         * The current selected note from
+         * the user, ready to edit.
+         * @type {object}
+         * @private
+         */
+        var _note = null;
+
+        /**
+         * All view elements managed by the
+         * editor like notes list, buttons and so on
+         * @type {{noteItemTemplate: null, $main: $main, $notesList: $notesList, $noteItemTemplate: $noteItemTemplate, $noteEditor: $noteEditor, $noteTitle: $noteTitle, $noteText: $noteText, $saveBtn: $saveBtn, $deleteBtn: $deleteBtn, $cancelBtn: $cancelBtn, $newBtn: $newBtn, $exportBtn: $exportBtn, $importBtn: $importBtn, $toDocumentBtn: $toDocumentBtn, $notesCommandBar: $notesCommandBar, $search: $search, $infoMessage: $infoMessage, $fileUpload: $fileUpload}}
+         */
         var ui = {
             noteItemTemplate:null,
+
             $main:function () {
                 return $(selector);
             },
@@ -109,6 +163,12 @@ define(function(require) {
             _stateList();
         }
 
+        /**
+         * Sets the view on state "display notes list".
+         * Loads the notes from local storage and renders
+         * the notes. Then updates all ui element accordingly.
+         * @private
+         */
         function _stateList(){
             _note = null;
             _readData(function () {
@@ -137,6 +197,12 @@ define(function(require) {
             });
         }
 
+        /**
+         * Sets the view on editing state
+         * by displaying the editor form,
+         * and hiding the list of notes.
+         * @private
+         */
         function _stateNewNote() {
             _note = null;
             ui.$noteEditor().show();
@@ -144,6 +210,12 @@ define(function(require) {
             ui.$notesList().hide();
         }
 
+        /**
+         * Sets the view on editing state
+         * by displaying the editor form,
+         * and hiding the list of notes.
+         * @private
+         */
         function _stateEditNote(ev) {
             _note = ev.data.note;
             ui.$noteTitle().val(_note.title);
@@ -154,6 +226,12 @@ define(function(require) {
             ui.$notesList().hide();
         }
 
+        /**
+         * Validates the form data and
+         * add/update the editing note.
+         * Then reload the list of notes.
+         * @private
+         */
         function _onSave(){
 
             var title = ui.$noteTitle().val();
@@ -175,12 +253,18 @@ define(function(require) {
                        text:ui.$noteText().val()
                    });
 
+               //saved, now reloads the list of notes
                _saveData(_stateList);
            }
            else
                alert(_notesExtConfig.validationWarn);
         }
 
+        /**
+         * Deletes the current editing
+         * note from the local storage
+         * @private
+         */
         function _onDelete() {
             if(_note){
                 _notes.splice(_note.index,1);
@@ -188,6 +272,16 @@ define(function(require) {
             }
         }
 
+        /**
+         * Converts the notes object to text
+         * string and saves it into a link
+         * url in the view. The link is set
+         * to force the download of the
+         * resource, so the string content
+         * gets downloaded by the user as
+         * a text file (backup purposes)
+         * @private
+         */
         function _onExport() {
             var href = ui.$exportBtn().attr('data-href');
 
@@ -198,7 +292,17 @@ define(function(require) {
 
             ui.$exportBtn().attr('href', href);
         }
-        
+
+        /**
+         * Converts the notes object to HTML
+         * code string, and saves it into a link
+         * url in the view. The link is set
+         * to force the download of the
+         * resource, so the string content
+         * gets downloaded by the user as
+         * an HTML file (for printing)
+         * @private
+         */
         function _onExportToDocument() {
             var href = ui.$toDocumentBtn().attr('data-href');
 
@@ -215,11 +319,25 @@ define(function(require) {
             ui.$toDocumentBtn().attr('href', href);
         }
 
+        /**
+         * Filters the list of notes displayed
+         * on view by the searching keys.
+         * Keys must be separated by white space,
+         * search is performed in all note fields
+         * and it is case-insensitive.
+         * @private
+         */
         function _onSearch() {
+
+            //when keys are available runs the search
             var searchValue = ui.$search().val();
             if(searchValue){
                 var keys = searchValue.split(' ');
 
+                //makes a list of filtered notes
+                //and displays them instead of the
+                //original once, if the filter
+                //returns some value.
                 var results = [];
                 $(keys).each(function (i,key) {
                     var filtered = $.grep(_notes,function (note,i) {
@@ -235,17 +353,30 @@ define(function(require) {
                 if(results.length>0)
                     _renderNotes(results);
             }
+            //when no keys, resets the filter
+            // and displays all notes
             else
                 _renderNotes(_notes);
         }
 
+        /**
+         * This function is invoked when the user
+         * requests to "upload" a notes backup file
+         * from the device. Any existing note will
+         * be replaced with the backup.
+         * @private
+         */
         function _onUpload(){
+            //reads the file from user device
             var files = ui.$fileUpload()[0].files;
             if (files && files[0]) {
                 var reader = new FileReader();
                 reader.onload = function (e) {
                     try{
                         if(e.target.result && e.target.result.trim().length>0){
+                            //decodes the base64 content of the backup
+                            //updates teh notes in the local storage
+                            //and on view
                             var content = e.target.result.replace(BASE64_PREFIX,'');
                             content = Base64.decode(content);
                             content = JSON.parse(content);
@@ -262,6 +393,15 @@ define(function(require) {
             }
         }
 
+        /**
+         * Renders the list of given notes
+         * to the view template and registers
+         * an event listener on each note item
+         * to display the note editing form
+         * on selection.
+         * @param notesList {object[]}
+         * @private
+         */
         function _renderNotes(notesList) {
             ui.$notesList().empty();
             $(notesList).each(function (i,note) {
@@ -278,39 +418,71 @@ define(function(require) {
             });
         }
 
+        /**
+         * Reads notes from the browser local storage.
+         * @param callback {function}
+         * @return {function}
+         * @private
+         */
         function _readData(callback) {
-            var storedNotes = localStorage.getItem(STORAGE_KEY);
-            _notes = JSON.parse(storedNotes) || [];
-            return callback();
+            try{
+                var storedNotes = localStorage.getItem(STORAGE_KEY);
+                _notes = JSON.parse(storedNotes) || [];
+                return callback();
+            }
+            catch (err){
+                alert('Error reading the notes on browser storage.');
+                console.error(err);
+            }
         }
 
+        /**
+         * Saves the notes on browser local storage.
+         * @param callback {function}
+         * @return {function}
+         * @private
+         */
         function _saveData(callback) {
-            localStorage.setItem(STORAGE_KEY,JSON.stringify(_notes));
-            return callback();
+            try{
+                localStorage.setItem(STORAGE_KEY,JSON.stringify(_notes));
+                return callback();
+            }
+            catch (err){
+                alert('Could not save notes to browser storage. Please check the storage quota.');
+                console.error(err);
+            }
         }
 
 
         constructor();
     };
 
-
+    //acts just as a wrapper around the
+    //NoteEditor done with JQuery
     return Backbone.View.extend({
         className: "adaptNotes",
 
+        //basic ini available for all plugins
+        //like this one
         initialize: function() {
             this.listenTo(Adapt, 'remove', this.remove);
             this.render();
         },
 
+        //loads the UI template from .hbs file
+        //and renders it.
         render: function() {
             this.$el.html(Handlebars.templates["notes"]());
             _.defer(_.bind(this.postRender, this));
             return this;
         },
 
+        //Here actually make the plugin working
+        //since the NotesEditor object is the one
+        //that hosts all the logic needed.
         postRender: function() {
             this.listenTo(Adapt, 'drawer:triggerCustomView', this.remove);
-            $Notes(Adapt,('.'+this.className));
+            NotesEditor(Adapt,('.'+this.className));
         }
     });
 });
